0683a103e68882de0a532428415849f76ca1c36c
Pernell Bauza <pernellbauza@gmail.com>
2023-07-09 10:52:17 -0700

    edit modal updated

--- scripts/index.js
+++ scripts/index.js
@@ -26,3 +26,16 @@ const initialCards = [
 ]
 
 console.log(initialCards);
+
+
+/* Elements*/
+const profileEditButton = document.querySelector('#profile__edit-button');
+const profileEditModal = document.querySelector('#profile__edit-modal');
+const modalCloseButton = document.querySelector('#modal__close-button')
+
+profileEditButton.addEventListener('click' , () => {
+  profileEditModal.classList.add('modal__opened')
+})
+modalCloseButton.addEventListener("click", () => {
+  profileEditModal.classList.remove("modal__opened");
+});
